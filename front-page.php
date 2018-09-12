ff09fd5edf695e62a09ecf14ee467d629f31cc7f
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-09-12 15:00:44 -0300

    Adequação ao Bootstrap 4 e padronização

--- front-page.php
+++ front-page.php
@@ -2,8 +2,10 @@
 
 <?php if (is_active_sidebar('widget-home')) : ?>
 <div class="row">
-    <div class="col-xs-12">
-        <?php if (!dynamic_sidebar('widget-home')) : endif; ?>
+    <div class="col-12 col-lg-10 offset-lg-1">
+        <div class="area-home">
+            <?php if (!dynamic_sidebar('widget-home')) : endif; ?>
+        </div>
     </div>
 </div>
 <?php endif; ?>
@@ -19,25 +21,27 @@
 
 <div class="row">
     <?php while ($query->have_posts() && $query->current_post < 1) : $query->the_post(); ?>
-        <div class="col-xs-12 col-md-6">
+        <div class="col-12 col-lg-6">
             <article class="noticia">
                 <?php get_template_part('partials/noticias/item'); ?>
             </article>
         </div>
     <?php endwhile; ?>
-    <div class="col-xs-12">
+    <div class="col-12">
         <?php wp_reset_query(); ?>
-        <hr class="separador-noticia">
-        <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>" class="pull-right link-todas-noticias"><?php _e('Acesse mais notícias'); ?></a>
+        <div class="acesso-todas-noticias">
+            <hr class="acesso-todas-noticias__separador">
+            <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>" class="float-right acesso-todas-noticias__link"><?php _e('Acesse mais notícias'); ?></a>
+        </div>
     </div>
 </div>
 
 <?php if (is_active_sidebar('widget-docs') || is_active_sidebar('widget-home-side')) : ?>
 <div class="row">
-    <div class="col-xs-12<?php echo is_active_sidebar('widget-home-side') ? ' col-md-6' : ''; ?>">
+    <div class="col-12<?php echo is_active_sidebar('widget-home-side') ? ' col-lg-6' : ''; ?>">
         <?php if (!dynamic_sidebar('widget-docs')) : endif; ?>
     </div>
-    <div class="col-xs-12<?php echo is_active_sidebar('widget-docs') ? ' col-md-6' : ''; ?>">
+    <div class="col-12<?php echo is_active_sidebar('widget-docs') ? ' col-lg-6' : ''; ?>">
         <?php if (!dynamic_sidebar('widget-home-side')) : endif; ?>
     </div>
 </div>
@@ -45,7 +49,7 @@
 
 <?php if (is_active_sidebar('widget-gallery')) : ?>
 <div class="row">
-    <div class="col-xs-12">
+    <div class="col-12">
         <?php if (!dynamic_sidebar('widget-gallery')) : endif; ?>
     </div>
 </div>
@@ -53,7 +57,7 @@
 
 <?php if (is_active_sidebar('widget-atalhos')) : ?>
 <div class="row">
-    <div class="col-xs-12">
+    <div class="col-12">
         <h2 class="title-box"><?php _e('Acesso R&aacute;pido'); ?></h2>
         <nav>
             <ul class="area-atalhos">
@@ -68,8 +72,11 @@
 <hr class="banner-separator">
 
 <div class="row">
-    <div class="col-xs-12">
-        <?php if (!dynamic_sidebar('widget-banners')) : endif; ?>
+    <div class="col-12">
+        <div class="area-banners">
+            <hr class="area-banners__separator">
+            <?php if (!dynamic_sidebar('widget-banners')) : endif; ?>
+        </div>
     </div>
 </div>
 <?php endif; ?>
--- partials/header-title.php
+++ partials/header-title.php
@@ -1,21 +1,21 @@
 <div class="row">
 <?php if (get_header_image() != '') : ?>
-    <div class="col-xs-12">
+    <div class="col-12">
         <h1 class="sr-only"><?php bloginfo('name'); ?></h1>
-        <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php header_image(); ?>" alt="<?php bloginfo('name'); ?>" class="img-responsive"/></a>
+        <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php header_image(); ?>" alt="<?php bloginfo('name'); ?>" class="img-fluid"/></a>
     </div>
 <?php else: ?>
-    <div class="col-xs-12 col-sm-2 visible-sm visible-md visible-lg">
-        <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/if.png" alt="Logo do IF" class="center-block"></a>
+    <div class="col-12 col-md-2 d-none d-md-block">
+        <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/if.png" alt="Logo do IF" class="mx-auto"></a>
     </div>
-    <div class="col-xs-12 col-sm-10">
+    <div class="col-12 col-md-10">
         <div class="row">
-            <div class="col-xs-12">
+            <div class="col-12">
                 <p class="title-ifrs"><?php _e('Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul'); ?></p>
             </div>
         </div>
         <div class="row">
-            <div class="col-xs-12">
+            <div class="col-12">
                 <h1 class="title-site"><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
             </div>
         </div>
