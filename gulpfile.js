e05e82125eaa4380401d4e5b4cf667ded04f9860
CTA <cta@ifrs.edu.br>
2019-02-20 10:14:46 -0300

    Remoção de código comentado

--- gulpfile.js
+++ gulpfile.js
@@ -55,11 +55,6 @@ gulp.task('sass', function() {
     .pipe(browserSync.stream());
 });
 
-// gulp.task('vendor-css', function () {
-//     return gulp.src(['vendor-css/*.css'])
-//            .pipe(gulp.dest('css/'));
-// });
-
 gulp.task('styles', gulp.series('sass', function css() {
     return gulp.src(['css/*.css', '!css/*.min.css'])
     .pipe(cssmin())
