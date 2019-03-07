c6f5745eca54264ecd41ae9d665464c20534d559
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-03-07 17:08:26 -0300

    Adição de controle de cache nos assets

--- inc/assets.php
+++ inc/assets.php
@@ -5,9 +5,9 @@ function cta_load_css_js() {
         wp_deregister_script('jquery');
     }
 
-    wp_enqueue_style('css-cta', get_stylesheet_directory_uri().(WP_DEBUG ? '/css/cta.css' : '/css/cta.min.css'), array(), false, 'all');
+    wp_enqueue_style('css-cta', get_stylesheet_directory_uri().(WP_DEBUG ? '/css/cta.css' : '/css/cta.min.css'), array(), WP_DEBUG ? false : filemtime(get_stylesheet_directory() . '/css/cta.min.css'), 'all');
 
-    wp_enqueue_script('repositorioTAjs', get_theme_file_uri('/js/repositorioTA.js'), array(), false, true);
+    wp_enqueue_script('repositorioTAjs', get_theme_file_uri('/js/repositorioTA.js'), array(), WP_DEBUG ? false : filemtime(get_stylesheet_directory() . '/js/repositorioTA.js'), true);
 }
-add_action( 'wp_enqueue_scripts', 'cta_load_css_js');
 
+add_action( 'wp_enqueue_scripts', 'cta_load_css_js');
