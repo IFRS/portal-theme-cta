cd58c6ea35b39a882682aa5c2914a21b8e050041
CTA <cta@ifrs.edu.br>
2019-06-18 12:02:59 -0300

    Remoção de código

--- taxonomy-categorias-ta.php
+++ taxonomy-categorias-ta.php
@@ -1,14 +1,13 @@
 <?php get_header(); ?>
 
 <?php 
-    global $wp_query;
-    $query = $wp_query->query;
-    
     $_POST['filter_recursos'] = true;
     $_POST['filter_categorias_ta'] =  get_queried_object_id();
-    
-    
 ?>
 <?php get_template_part('partials/recursos-ta/list'); ?>
 
 <?php get_footer(); ?>
+
+
+
+
